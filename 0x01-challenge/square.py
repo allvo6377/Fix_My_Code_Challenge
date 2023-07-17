615149083cb66c2401a2837e9592f78732d0bbb3
allvo6377 <alvinpilot@gmail.com>
2023-07-17 23:29:21 +0000

    Executable

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,20 +1,34 @@
- def __init__(self, *args, **kwargs):
+#!/usr/bin/python3
+'''module:
+I love geometry! - file for a square class
+'''
+
+
+class Square():
+    '''repr for a square class'''
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        '''constructor to initialze attributes'''
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        '''perimeter of a geometric square'''
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        '''return string representation of square'''
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
